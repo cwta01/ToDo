5a4c255b9483c8b583c4385608997738154a82b9
cwta01 <cristiano.watanabe@gmail.com>
2019-09-10 19:53:52 -0300

    aula 12 e 13

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from .forms import TaskForm
+from django.contrib import messages
 
 from .models import Task
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    paginator = Paginator(tasks_list, 3)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
+
     return render(request,'tasks/list.html',{'tasks': tasks})
 
 def taskView(request, id):
@@ -40,6 +49,14 @@ def editTask(request, id):
             return render(request, 'tasks/edittask.html', {'form': form, 'task': task})    
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+def deleteTask(request, id):
+     task = get_object_or_404(Task, pk=id)
+     task.delete()
+
+     messages.info(request, 'Tarefa deletada com sucesso.')
+
+     return redirect('/')
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
